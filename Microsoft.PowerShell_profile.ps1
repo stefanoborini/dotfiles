d8c48408951bf11fd380bb73b7e87f190cbcd7ae
Stefano Borini <stefano.borini@gmail.com>
2018-09-20 13:18:12 +0100

    Added colors and svn status

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -5,10 +5,38 @@ set-alias mysql "C:\Program Files\MySQL\MySQL Server 8.0\bin\mysql.exe"
 set-alias sha256 "Get-FileHash"
 set-alias which "Get-Command"
 
+function color 
+{
+    param([string]$Color, [string]$str)
+
+    return (Write-Host -ForegroundColor $Color $str)
+}
+
 function prompt
 {
+    $svnpath=""
+    $svninfo=(svn info)
+    if ($LASTEXITCODE -eq 0) {
+        $match = $svninfo | Select-String "^URL"
+        if ($match) {
+            $svnpath = $match.Line.Split('/')[-1] 
+        }
+        $match = $svninfo | Select-String "^Revision"
+        if ($match) {
+            $svnrev = $match.Line.Split(' ')[-1] 
+        }
+    }
     Write-Host -nonewline "PS "
     Write-Host -nonewline $pwd.Path
+    Write-Host -nonewline " " 
+    if ($svnpath) {
+        Write-Host -nonewline -ForegroundColor red $svnpath
+        Write-Host -nonewline " " 
+    }
+    if ($svnrev) {
+        Write-Host -nonewline -ForegroundColor yellow $svnrev
+        Write-Host -nonewline " " 
+    }
     Write-Host -nonewline ">"
     return " " 
 }
