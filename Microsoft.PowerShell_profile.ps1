b932a3b41a0effc9f3c344b0d0c2c8a50402fb6f
Stefano Borini <stefano.borini@gmail.com>
2018-10-17 12:32:13 +0100

    More refinements to powershell script

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,16 +1,13 @@
-set-alias vim "C:\Program Files (x86)\Vim\vim74\vim.exe"
+set-alias vim "C:\Program Files\Git\usr\bin\vim.exe"
 set-alias make "mingw32-make"
 set-alias open "start"
 set-alias mysql "C:\Program Files\MySQL\MySQL Server 8.0\bin\mysql.exe"
 set-alias sha256 "Get-FileHash"
 set-alias which "Get-Command"
+set-alias hexdump "Format-Hex"
 
-function color 
-{
-    param([string]$Color, [string]$str)
-
-    return (Write-Host -ForegroundColor $Color $str)
-}
+# Set bash style autocompletion
+Set-PSReadlineKeyHandler -Key Tab -Function Complete
 
 function prompt
 {
@@ -19,11 +16,20 @@ function prompt
     if ($LASTEXITCODE -eq 0) {
         $match = $svninfo | Select-String "^URL"
         if ($match) {
-            $svnpath = $match.Line.Split('/')[-1] 
+            $svnpath_elems = $match.Line.Split('/')
+        }
+        $branches_idx = $svnpath_elems.IndexOf("branches")
+        $trunk_idx = $svnpath_elems.IndexOf("trunk")
+        if ($branches_idx -ge 0 -and $branches_idx -lt $svnpath_elems.Length - 1) {
+            $svnpath = $svnpath_elems[$branches_idx+1]
+        } elseif ($trunk_idx -ge 0) {
+            $svnpath = "trunk"
+        } else {
+            $svnpath = ""
         }
         $match = $svninfo | Select-String "^Revision"
         if ($match) {
-            $svnrev = $match.Line.Split(' ')[-1] 
+            $svnrev = $match.Line.Split(' ')[-1]
         }
     }
     Write-Host -nonewline "PS "
@@ -41,3 +47,21 @@ function prompt
     return " " 
 }
 
+$Env:SVN_EDITOR='"C:\Program Files\Git\usr\bin\vim.exe"'
+
+function home {
+    cd $Home
+}
+
+remove-item alias:\ls
+
+function ls {
+    $List = gci -path $pwd.Path
+    foreach ($item in $List) {
+        if ($item.PSIsContainer) {
+            write-host $item.Name -foregroundcolor "blue" 
+        } else {
+            write-host $item.Name 
+        }
+    }
+}
